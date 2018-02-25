b1ac41d6c260cbb8e7681715c9b812f9fe32bbfc
nurul_basitah <36689022+nurulbasitah@users.noreply.github.com>
2018-02-25 21:29:41 +0800

    Update MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -1,28 +1,27 @@
 package week1;
 
+public class MyThread extends Thread{
 
-public class MyThread  extends Thread{
+@override
+public void run() {
+System.out.println("running...");
+System.out.println("running...Nurul Basitah");
 
-
-   @Override
-   public void run() {
-        System.out.println("running...");
-        System.out.println("running...Nurul Basitah");
-        
-    }
-
-    public static void main(String args[]) {
-        MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
-        System.out.println("Name of t1:" + t1.getName());
-        System.out.println("Name of t2:" + t2.getName());
-        System.out.println("id of t1:" + t1.getId());
-
-        t1.start();
-        t2.start();
-        
-        System.out.println("running...240450");
-        t1.setName("STIW3054");
-        System.out.println("After changing name of t1:" + t1.getName());
-    }
 }
+
+public static void main(String args[]) {
+    MyThread t1 = new MyThread();
+    MyThread t2 = new MyThread();
+    System.out.println("Name of t1:" + t1.getName());
+    System.out.println("Name of t2:" + t2.getName());
+    System.out.println("id of t1:" + t1.getId());
+
+    t1.start();
+    t2.start();
+    
+    System.out.println("running...240450");
+    t1.setName("STIW3054");
+    System.out.println("After changing name of t1:" + t1.getName());
+}
+}
+
